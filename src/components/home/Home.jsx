a430ebd9ae9fccfc350c0d9535ae88970633591a
aucarr <aecarr2@github.com>
2022-02-27 16:45:28 -0600

    Updated landing page and title + fixed scrolling

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -12,11 +12,11 @@ export default function Home() {
     useEffect(()=>{
         init(textRef.current, {
           showCursor: true,
-          backDelay: 1500,
-          startDelay: 1500,
-          backSpeed: 60,
+          startDelay: 3000,
+          typeSpeed: 500,
           cursorChar: "_",
-          strings: ["Hello", "Hola", "Bonjour", "Ciao", "Guten tag", "Olá", "Kia Ora", "G’day", "Geia", "Zdravo", "Privet", "Nǐ hǎo", "Namaste", "Kon’nichiwa", "Merhaba", "Merhaba", "Anyeong Haseyo", "Ahoj", "Hallo", "Cześć", "S̄wạs̄dī", "Szia", "Ahoj", "Hyālō", "Assalam u Alaikum", "Salām", "Dobryj Den", "Hallå", "Hallo", "Bunâ", "Shalom", "Barev", "Marhabaan", "Sata Srī Akāla", "halo", "elamat Pagi", "Xin Chào", "Kaixo", "Servus", "Kamusta", "Jambo", "Salom", "Salam", "Slav", "Bonjou", "Dobry Dzień", "Moni", "Bula", "Aloha", "Tālofa", "Mālō e lelei", "A Gutn Tog", "S’mae", "Hei", "Sveiki", "Sveika/Sveiks", "Halló", "Halò", "Dia Duit", "Tere", "Tashi Delek", "Sabaidee", "Gamarjoba", "Dumela", "Saluton", "Bongu", "Nde-ewo", "Moïen", "Ya’at’eeh", "Grüezi", "Ia Orana", "Përshëndetje", "Sain uu", "Salaam Aleekum", "Nóng gō", "Vanakkam", "Salam", "Namaskāra", "Sannu", "Mingalaba", "Selam", "Demat", "Akkam", "Khurumjari", "Kumusta", "Salama", "Henda ho", "Choum Reap Sor", "Ayubowan", "Salam Alaykum", "Li-hó", "Dumela", "Allianchu", "Molweni" ],
+          loop: false,
+          strings: ["AUSTIN CARR"],
         });
     },[])
 
